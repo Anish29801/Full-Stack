5313c6deb1c813d6053a54d89921eb1157c7e0e2
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2022-11-24 19:55:02 +0530

    Node Packages

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -31,7 +31,7 @@ exports.createUser = async (req, res) => {
 
 exports.getUsers = async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.find({});
     res.status(200).json({
       success: true,
       users,
--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
 
-connToDB;
+connToDB();
 app.use("/",routes);
 
 module.exports = app;
